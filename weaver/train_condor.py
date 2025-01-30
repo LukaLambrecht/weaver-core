1ece30104bbf030b5116d318ad4947ceec78b024
LukaLambrecht <Luka.Lambrecht@UGent.be>
2025-01-30 16:49:08 +0100

    add condor job submission

---
+++ weaver/train_condor.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import weaver.utils.condortools as ct
+
+
+if __name__=='__main__':
+
+    # read the weaver command from the command line
+    args = sys.argv[1:]
+
+    # conda environment settings
+    conda_activate = 'cd /eos/user/l/llambrec; source miniforge3/bin/activate'
+    conda_env = 'weaver'
+
+    # make the full weaver command
+    cmd = 'weaver ' + ' '.join(args)
+    print('Submitting the following command:')
+    print(cmd)
+
+    # submit the job
+    ct.submitCommandAsCondorJob('cjob_weaver', cmd,
+      jobflavour='workday', conda_activate=conda_activate, conda_env=conda_env)
