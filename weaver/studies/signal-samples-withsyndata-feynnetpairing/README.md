947f5538620d1950060d09f91d793f72f807107b
LukaLambrecht <Luka.Lambrecht@ugent.be>
2025-07-21 09:44:59 -0400

    move feynnetpairing studies to separate folder

---
+++ weaver/studies/signal-samples-withsyndata-feynnetpairing/README.md
@@ -0,0 +1,5 @@
+# Study the effect of training on synthetic data instead of simulation
+
+Comes on top of baseline + resampling + extra signal samples.
+The signal samples and synthetic data have been ntupled using FeynNet for pairing the jets,
+rather than the default MinDiag method, to see how it affects the background sculpting.
--- weaver/studies/signal-samples-withsyndata-feynnetpairing/run.py
+++ weaver/studies/signal-samples-withsyndata-feynnetpairing/run.py
@@ -16,16 +16,14 @@ if __name__=='__main__':
     weaverdir = os.path.join(weavercoredir, 'weaver')
     # data config
     data_config = os.path.abspath('configs/data_config_pnet_train_syndata_test_syndata.yaml')
-    #data_config = os.path.abspath('configs/data_config_pnet_train_syndata_test_bkg.yaml')
     # model config
     model_config = os.path.abspath('configs/model_pnet.py')
     # sample list for training data
-    sample_config_train = os.path.abspath('configs/samples_hh4b_multimh_vs_syndata_training.yaml')
+    sample_config_train = os.path.abspath('configs/samples_hh4b_multimh_vs_syndata_training_feynnetpairing.yaml')
     # sample list for testing data
-    sample_config_test = os.path.abspath('configs/samples_hh4b_multimh_vs_syndata_testing.yaml')
-    #sample_config_test = os.path.abspath('configs/samples_hh4b_multimh_vs_bkg_allyears_testing.yaml')
+    sample_config_test = os.path.abspath('configs/samples_hh4b_multimh_vs_syndata_testing_mindiagpairing.yaml')
     # output dir
-    outputdir = os.path.join(thisdir, 'output_test')
+    outputdir = os.path.join(thisdir, 'output_withextrasigsamples_mindiagtesting')
     # network settings
     num_epochs = 30
     steps_per_epoch = 300
@@ -36,7 +34,7 @@ if __name__=='__main__':
     runmode = 'slurm'
     #conda_activate = 'source /eos/user/l/llambrec/miniforge3/bin/activate'
     #conda_env = 'weaver'
-    slurmscript = 'sjob_test.sh'
+    slurmscript = 'sjob_weaver_withextrasigsamples_mindiagtesting.sh'
     env_cmds = ([
         'env_path=/blue/avery/llambre1.brown/miniforge3/envs/weaver/bin/',
         'export PATH=$env_path:$PATH',
